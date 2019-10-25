2166c20d3335f81482ab7bfb851616fad6c973aa
ThaiDH <thaidh@ecorau.com>
2019-10-25 17:48:15 +0700

    Add swagger api model

--- base-rest-api/src/main/java/com/kitcut/helloworld/baserestapi/dto/request/employee/EmployeeCreateRequest.java
+++ base-rest-api/src/main/java/com/kitcut/helloworld/baserestapi/dto/request/employee/EmployeeCreateRequest.java
@@ -1,16 +1,30 @@
 package com.kitcut.helloworld.baserestapi.dto.request.employee;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Getter
 @Setter
+@ApiModel(description = "All details about the Employee. ")
 public class EmployeeCreateRequest {
+    @ApiModelProperty(notes = "Employee Name")
+    @Size(min = 4, max = 50)
     @NotEmpty
     private String name;
+
+    @ApiModelProperty(notes = "Employee Birthday")
+    @NotNull
     private Date birthday;
+
+    @ApiModelProperty(notes = "Employee Address")
+    @NotBlank
     private String address;
+
+    @Pattern(regexp = ".+@.+\\..+", message = "Please provide a valid email address")
+    private String email;
 }
--- base-rest-api/src/main/java/com/kitcut/helloworld/baserestapi/dto/response/employee/EmployeeCreateResponse.java
+++ base-rest-api/src/main/java/com/kitcut/helloworld/baserestapi/dto/response/employee/EmployeeCreateResponse.java
@@ -2,6 +2,8 @@ package com.kitcut.helloworld.baserestapi.dto.response.employee;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.kitcut.helloworld.baserestapi.config.JsonDateSerializer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,10 @@ import java.util.Date;
 
 @Getter
 @Setter
+@ApiModel(description = "All details about the Employee. ")
 public class EmployeeCreateResponse {
     private Long id;
+    @ApiModelProperty(notes = "Employee Name")
     private String name;
     private Date birthday;
     private String address;
