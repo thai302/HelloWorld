be3bd66f7d69dde6c75981a8707b6fdd06fcaadc
ThaiDH <thaidh@ecorau.com>
2019-11-14 17:21:01 +0700

    Add spring cloud auth

---
+++ spring-cloud-auth/src/test/java/com/kitcut/helloworld/springcloudauth/SpringCloudAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitcut.helloworld.springcloudauth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
